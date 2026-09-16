8c63f4a5df72e3f6c0b4f314632d053c9174894e
Aryan Singh <224284925+aryan7095@users.noreply.github.com>
2026-09-16 17:23:24 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+// Establishes a connection to MongoDB using the URI from environment variables.
+// Called once at server startup.
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
