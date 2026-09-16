bf0156df37516b2e91605be8aa61d0a99b7726bc
Aryan Singh <224284925+aryan7095@users.noreply.github.com>
2026-09-16 17:29:16 +0530

    Enhance comments in Applicants component
    Added comments for clarity on job applicants page.

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -7,15 +7,19 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAllApplicants } from '@/redux/applicationSlice';
 
+// Admin page showing all applicants for a specific job
 const Applicants = () => {
-    const params = useParams();
+    const params = useParams(); // job ID from the URL
     const dispatch = useDispatch();
+    // Applicants data (job + its applications) from Redux state
     const {applicants} = useSelector(store=>store.application);
 
+    // Fetch the job's applicants on mount
     useEffect(() => {
         const fetchAllApplicants = async () => {
             try {
                 const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
+                // Store the job object (which includes its populated applications) in Redux
                 dispatch(setAllApplicants(res.data.job));
             } catch (error) {
                 console.log(error);
@@ -27,7 +31,9 @@ const Applicants = () => {
         <div>
             <Navbar />
             <div className='max-w-7xl mx-auto'>
+                {/* Shows the total number of applicants for this job */}
                 <h1 className='font-bold text-xl my-5'>Applicants {applicants?.applications?.length}</h1>
+                {/* Table component that reads applicant details from Redux state */}
                 <ApplicantsTable />
             </div>
         </div>
